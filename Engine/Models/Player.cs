90295537ad795f2f78187e9ad95dd44fac871bcf
bartoszpasiecznik <40137554+bartoszpasiecznik@users.noreply.github.com>
2026-03-03 17:41:55 +0100

    Added Grid for displaying player stats, added eventhandler to update properites in Player class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,11 +1,56 @@
-﻿namespace Engine.Models;
+﻿using System.ComponentModel;
 
-public class Player
+namespace Engine.Models;
+
+public class Player : INotifyPropertyChanged
 {
-    public string Name {get;set;}
-    public string CharacterClass { get; set; }
-    public int HitPoints { get; set; }
-    public int ExperiencePoints { get; set; }
-    public int Level { get; set; }
-    public int Gold { get; set; }
+    private string _name;
+    private string _characterClass;
+    private int _hitPoints;
+    private int _experiencePoints;
+    private int _level;
+    private int _gold;
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; OnPropoertyChanged(nameof(Name)); }
+    }
+
+    public string CharacterClass
+    {
+        get  { return _characterClass; } 
+        set{ _characterClass = value; OnPropoertyChanged(nameof(CharacterClass)); }
+    }
+
+    public int HitPoints
+    {
+        get {return _hitPoints;}
+        set  { _hitPoints = value; OnPropoertyChanged(nameof(HitPoints)); }
+    }
+
+    public int ExperiencePoints
+    {
+        get{return _experiencePoints;}
+        set{_experiencePoints = value; OnPropoertyChanged(nameof(ExperiencePoints)); }
+    }
+
+    public int Level
+    {
+        get{return _level;}
+        set{ _level = value; OnPropoertyChanged(nameof(Level)); }
+    }
+
+    public int Gold
+    {
+        get{return _gold;}
+        set{ _gold = value; OnPropoertyChanged(nameof(Gold)); }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropoertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -3,12 +3,16 @@ namespace Engine.ViewModels;
 
 public class GameSession
 {
-    Player CurrentPlayer { get; set; }
+    public Player CurrentPlayer { get; set; }
 
     public GameSession()
     {
         CurrentPlayer = new Player();
         CurrentPlayer.Name = "The Bill";
+        CurrentPlayer.CharacterClass = "Fighter";
+        CurrentPlayer.HitPoints = 10 ;
         CurrentPlayer.Gold = 1000000;
+        CurrentPlayer.ExperiencePoints = 0;
+        CurrentPlayer.Level = 1;
     }
 }
