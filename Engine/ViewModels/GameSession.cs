222c59a18b20da8bfd26643a12bdecc50be9edfa
bartoszpasiecznik <40137554+bartoszpasiecznik@users.noreply.github.com>
2026-03-03 15:02:49 +0100

    Added Player and GameSession class

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,14 @@
+﻿using Engine.Models;
+namespace Engine.ViewModels;
+
+public class GameSession
+{
+    Player CurrentPlayer { get; set; }
+
+    public GameSession()
+    {
+        CurrentPlayer = new Player();
+        CurrentPlayer.Name = "The Bill";
+        CurrentPlayer.Gold = 1000000;
+    }
+}
