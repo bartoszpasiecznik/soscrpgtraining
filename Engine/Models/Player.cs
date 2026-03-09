091429c790de3b0b591d16ba89b7f4545f92e5ed
bartoszpasiecznik <40137554+bartoszpasiecznik@users.noreply.github.com>
2026-03-09 17:44:33 +0100

    BaseNotificationClass.cs added, refractored code, added guard causes, made WorldFactory.cs static

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -2,7 +2,7 @@
 
 namespace Engine.Models;
 
-public class Player : INotifyPropertyChanged
+public class Player : BaseNotificationClass
 {
     private string _name;
     private string _characterClass;
@@ -14,43 +14,37 @@ public class Player : INotifyPropertyChanged
     public string Name
     {
         get { return _name; }
-        set { _name = value; OnPropoertyChanged(nameof(Name)); }
+        set { _name = value; OnPropertyChanged(nameof(Name)); }
     }
 
     public string CharacterClass
     {
         get  { return _characterClass; } 
-        set{ _characterClass = value; OnPropoertyChanged(nameof(CharacterClass)); }
+        set{ _characterClass = value; OnPropertyChanged(nameof(CharacterClass)); }
     }
 
     public int HitPoints
     {
         get {return _hitPoints;}
-        set  { _hitPoints = value; OnPropoertyChanged(nameof(HitPoints)); }
+        set  { _hitPoints = value; OnPropertyChanged(nameof(HitPoints)); }
     }
 
     public int ExperiencePoints
     {
         get{return _experiencePoints;}
-        set{_experiencePoints = value; OnPropoertyChanged(nameof(ExperiencePoints)); }
+        set{_experiencePoints = value; OnPropertyChanged(nameof(ExperiencePoints)); }
     }
 
     public int Level
     {
         get{return _level;}
-        set{ _level = value; OnPropoertyChanged(nameof(Level)); }
+        set{ _level = value; OnPropertyChanged(nameof(Level)); }
     }
 
     public int Gold
     {
         get{return _gold;}
-        set{ _gold = value; OnPropoertyChanged(nameof(Gold)); }
+        set{ _gold = value; OnPropertyChanged(nameof(Gold)); }
     }
     
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropoertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,7 +4,7 @@ using Engine.Models;
 using Engine.Factories;
 namespace Engine.ViewModels;
 
-public class GameSession : INotifyPropertyChanged
+public class GameSession : BaseNotificationClass
 {
     private Location _currentLocation;
     public Player CurrentPlayer { get; set; }
@@ -42,42 +42,47 @@ public class GameSession : INotifyPropertyChanged
 
     public GameSession()
     {
-        CurrentPlayer = new Player();
-        CurrentPlayer.Name = "The Bill";
-        CurrentPlayer.CharacterClass = "Fighter";
-        CurrentPlayer.HitPoints = 10 ;
-        CurrentPlayer.Gold = 1000000;
-        CurrentPlayer.ExperiencePoints = 0;
-        CurrentPlayer.Level = 1;
-        
-        WorldFactory factory = new WorldFactory();
-        CurrentWorld = factory.CreateWorld();
+        CurrentPlayer = new Player
+        {
+            Name = "The Bill",
+            CharacterClass = "Fajter", 
+            HitPoints = 10, 
+            Gold = 100000, 
+            ExperiencePoints = 0, 
+            Level = 1
+        };
 
+        CurrentWorld = WorldFactory.CreateWorld();
         CurrentLocation = CurrentWorld.LocationAt(0, 0);
     }
 
     public void MoveNorth()
     {
-        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        if (HasLocationToNorth)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
     }
     public void MoveWest()
     {
-        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        if (HasLocationToWest)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
     }
     public void MoveEast()
     {
-        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        if (HasLocationToEast)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
     }
     public void MoveSouth()
     {
-        CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        if (HasLocationToSouth)
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
     }
     
 }
